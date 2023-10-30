277ab431f4d5c3b83508e70487f658633d877927
subham007-coder <dsubham109@gmail.com>
2023-10-30 22:21:05 +0530

    allmost done

---
+++ script.js
@@ -0,0 +1,33 @@
+let url = "http://universities.hipolabs.com/search?name=";
+
+
+let btn = document.querySelector("button");
+btn.addEventListener("click", async () => {
+    let country = document.querySelector("input").value;
+
+    let colleges = await getUniversity(country);
+
+    Show(colleges);
+});
+
+function Show(colleges){
+    colleges.forEach(collArr => {
+        let ul = document.querySelector("#res")
+
+        let makeLi = document.createElement("li");
+        makeLi.textContent = collArr.name;
+        ul.append(makeLi);
+        
+        console.log(collArr.name);
+        console.log(collArr.web_pages);
+    });
+}
+
+async function getUniversity(country) {
+    try {
+        let res = await axios.get(url + country);
+        return res.data;
+    } catch (err) {
+        return [];
+    }
+}
