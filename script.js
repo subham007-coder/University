9aadd1fc4b1c8110c991c2645cb24af0c1fc22a1
subham007-coder <dsubham109@gmail.com>
2023-10-30 22:37:33 +0530

    done

--- script.js
+++ script.js
@@ -4,20 +4,21 @@ let url = "http://universities.hipolabs.com/search?name=";
 let btn = document.querySelector("button");
 btn.addEventListener("click", async () => {
     let country = document.querySelector("input").value;
-
     let colleges = await getUniversity(country);
 
     Show(colleges);
 });
 
 function Show(colleges){
+    let ul = document.querySelector("#res");
+    ul.innerText = "";
+
     colleges.forEach(collArr => {
-        let ul = document.querySelector("#res")
 
         let makeLi = document.createElement("li");
         makeLi.textContent = collArr.name;
         ul.append(makeLi);
-        
+
         console.log(collArr.web_pages);
     });
 }
